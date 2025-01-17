5a56dd19749502d80b4453c02ba60fe19b23e818
SciMind2460 <saarthkarkera@gmail.com>
2025-01-17 17:14:44 +0530

    Unit testing

--- tests/unittests.py
+++ tests/unittests.py
@@ -1,10 +1,11 @@
 import unittest
 
 
-class CheckForWeekend(unittest.TestCase):
-    def test_weekend(self):
-        self.assertEqual(True, False)  # add assertion here
-
+class UnitTests(unittest.TestCase):
+    def test_check_for_weekend(self) :
+        for weekday in day_update.day_update.weekdays:
+            self.assertEqual(day_update.check_for_weekend(weekday.name.lower()), weekday.value)
+            self.assertEqual(day_update.check_for_weekend(weekday.name.upper()), weekday.value)
 
 if __name__ == '__main__':
     unittest.main()

c0e2f349cf34956a20c687e6ffc8f0cb42fe3be5
SciMind2460 <saarthkarkera@gmail.com>
2025-01-18 16:21:48 +0530

    Change imports

--- tests/test_check_for_weekend.py
+++ tests/test_check_for_weekend.py
@@ -3,7 +3,7 @@ import day_update
 
 class TestCheckWeekend(unittest.TestCase):
     def test_check_for_weekend(self) :
-        for weekday in day_update.day_update.weekdays:
+        for weekday in day_update.weekdays:
             self.assertEqual(day_update.check_for_weekend(weekday.name.lower()), weekday.value)
             self.assertEqual(day_update.check_for_weekend(weekday.name.upper()), weekday.value)
 
